caba37fedee782f260f12da63c49cc1622b07b41
Artem Egorov <artegtor@gmail.com>
2023-05-17 17:28:42 +0400

    init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# list-users
-empty
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="aside">Hello</div>
+      <div className="content">XXX</div>
+    </>
+  )
+}
+
+export default App
