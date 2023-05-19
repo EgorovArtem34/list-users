fcf310518c882989cf4134d2bcd0e1ab8fe1be98
Artem Egorov <artegtor@gmail.com>
2023-05-19 13:06:13 +0400

    update aside: layout

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -3,7 +3,17 @@ import './aside.scss';
 
 const Aside: React.FC = () => {
   return (
-    <div className="aside">Hello</div>
+    <div className="aside">
+      <div className="aside__container">
+        <p>Сортировка</p>
+        <button type='button' className="btn" onClick={() => console.log('')}>
+          по городу
+        </button>
+        <button type='button' className="btn" onClick={() => console.log('')}>
+          по компании
+        </button>
+      </div>
+    </div>
   )
 }
 
