27c27ec5fc596a11575fec1094cd27f10e3840de
Artem Egorov <artegtor@gmail.com>
2023-05-20 13:28:03 +0400

    update App.tsx: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import Aside from './components/Aside/Aside';
+import { Routes, Route } from 'react-router-dom';
 import UsersPage from './components/UsersPage/UsersPage';
+import UserInfo from './components/UserInfo/UserInfo';
+import routes from './routes';
 
 function App() {
-
   return (
-    <>
-      <Aside />
-      <UsersPage />
-    </>
+    <Routes>
+      <Route path={routes.defaultPath()} element={<UsersPage />} />
+      <Route path={routes.userPath()} element={<UserInfo />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
