afea2ac0f74f76712e7f2a4dc971ed7ab5466f17
Artem Egorov <artegtor@gmail.com>
2023-05-20 17:45:03 +0400

    update UsersPage : add loading, NavLink, count Users

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -8,14 +8,9 @@ import Aside from '../Aside/Aside';
 function Content() {
   const dispatch = useAppDispatch();
   const { users, isLoading } = useAppSelector((state) => state.usersSlice);
-
   useEffect(() => {
     dispatch(fetchUsers());
-  }, [dispatch])
-
-  if (isLoading) {
-    console.log('LOADING');
-  }
+  }, [dispatch]);
 
   return (
     <>
@@ -24,32 +19,36 @@ function Content() {
         <div className="container__header">
           <h3 className="container__title">Список пользователей</h3>
         </div>
-        {users.map((user) => (
-          <div className="user-card" key={user.id}>
-            <div className="user-card__container">
-              <p>
-                <span>ФИО: </span>
-                {user.name}
-              </p>
-              <p>
-                <span>город: </span>
-                {user.address.city}
-              </p>
-              <div className="user-card__last-line">
+        {isLoading && <p>Loading...</p>}
+        <div className="container__users users">
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>
+              <div className="user-card__container">
                 <p>
-                  <span>компания: </span>
-                  {user.company.name}
+                  <span>ФИО: </span>
+                  {user.name}
                 </p>
-                <NavLink
-                  onClick={() => dispatch(setActiveUser(user.id))}
-                  to={`user/${user.id}`}
-                >
-                  Подробнее
-                </NavLink>
+                <p>
+                  <span>город: </span>
+                  {user.address.city}
+                </p>
+                <div className="user-card__last-line">
+                  <p>
+                    <span>компания: </span>
+                    {user.company.name}
+                  </p>
+                  <NavLink
+                    onClick={() => dispatch(setActiveUser(user.id))}
+                    to={`user/${user.id}`}
+                  >
+                    Подробнее
+                  </NavLink>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+          {!isLoading && <p className="users__text">Найдено {users.length} пользователей</p>}
+        </div>
       </div>
     </>
   );
